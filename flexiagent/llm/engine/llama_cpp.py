9a19b65c790c6307dc0e7adc9bff117ee9173cd8
Hiram Deng <72371329+dzhsurf@users.noreply.github.com>
2024-09-28 02:31:51 -0700

    Fix llama_cpp structured output empty issue.

--- flexiagent/llm/engine/llama_cpp.py
+++ flexiagent/llm/engine/llama_cpp.py
@@ -9,10 +9,16 @@ from llama_cpp import (
     CreateChatCompletionResponse,
     Llama,
 )
+from pydantic import ValidationError
 
 from flexiagent.llm.engine.engine_base import LLMEngineConfig, LLMEngineImpl
 from flexiagent.llm.structured_schema import FxLLMStructuredSchema
-from flexiagent.prompts.prompt import PromptFewshotSample, PromptTemplate, PromptValue
+from flexiagent.prompts.prompt import (
+    PromptFewshotSample,
+    PromptInstTemplate,
+    PromptTemplate,
+    PromptValue,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -34,6 +40,7 @@ class LLMEngineLlamaCpp(LLMEngineImpl[LLMConfigLlamaCpp]):
                 model_path,
                 n_ctx=config.n_ctx if config.n_ctx else 512,
                 n_batch=config.n_ctx if config.n_ctx else 512,
+                chat_format="chatml",
                 verbose=False,
             )
         else:
@@ -42,6 +49,7 @@ class LLMEngineLlamaCpp(LLMEngineImpl[LLMConfigLlamaCpp]):
                 config.repo_filename,
                 n_ctx=config.n_ctx if config.n_ctx else 512,
                 n_batch=config.n_ctx if config.n_ctx else 512,
+                chat_format="chatml",
                 verbose=False,
             )
 
@@ -68,7 +76,7 @@ class LLMEngineLlamaCpp(LLMEngineImpl[LLMConfigLlamaCpp]):
         response = self.llm.create_chat_completion(
             temperature=0,
             messages=messages,
-            stop=prompt.stop_prompt,
+            stop=prompt.stop_prompt if len(prompt.stop_prompt) > 0 else None,
         )
         res = cast(CreateChatCompletionResponse, response)
 
@@ -84,49 +92,35 @@ class LLMEngineLlamaCpp(LLMEngineImpl[LLMConfigLlamaCpp]):
         variables: Dict[str, PromptValue] = {},
         response_format: Type[FxLLMStructuredSchema] = FxLLMStructuredSchema,
     ) -> FxLLMStructuredSchema:
-        # fields = response_format.__annotations__
-        # json_schema = [f"{name}: {_type}" for name, _type in fields.items()]
-        # json_schema_text = "\n".join(json_schema)
-        #         prompt.user_question_prompt += f"""
-        # Answer in JSON format:
-        # {json_schema_text}
-        # """
-
-        # prompt_text = prompt.to_text(
-        #     inst_template=PromptInstTemplate(),
-        #     variables=variables,
-        # )
-        # print(prompt_text)
-        # response = self.llm.create_completion(
-        #     prompt_text,
-        #     max_tokens=20000,
-        #     temperature=0,
-        #     stop=prompt.stop_prompt,
-        # )
-
         messages = [
             cast(ChatCompletionRequestMessage, msg)
             for msg in prompt.to_openai_chat_completion_messages(variables)
         ]
         logger.debug(messages)
-        print("\n\n", messages)
+
+        json_schema = response_format.model_json_schema()
 
         response = self.llm.create_chat_completion(
             temperature=0,
             messages=messages,
-            stop=prompt.stop_prompt,
-            # response_format=ChatCompletionRequestResponseFormat(type="json_object"),
+            stop=prompt.stop_prompt if len(prompt.stop_prompt) > 0 else None,
+            response_format={
+                "type": "json_object",
+                "schema": json_schema,
+            },
         )
 
-        print(type(response), response)
+        logger.debug(response)
 
-        return response
-        # res = cast(CreateChatCompletionResponse, response)
-        # res["choices"][0]["message"]["content"].
+        result_json_text = ""
+        res = cast(CreateChatCompletionResponse, response)
+        if len(res["choices"]) > 0 and res["choices"][0]["message"]["content"]:
+            result_json_text = res["choices"][0]["message"]["content"].strip()
 
-        # if len(res["choices"]) > 0 and res["choices"][0]["message"]["content"]:
-        #     return res["choices"][0]["message"]["content"].strip()
+        logger.debug(result_json_text)
+        try:
+            result_obj = response_format.model_validate_json(result_json_text)
+        except ValidationError:
+            raise TypeError(f"LLM Structured output failed. {result_json_text}")
 
-        # return ""
-
-        # raise ValueError("llama.cpp not support structured output.")
+        return result_obj
