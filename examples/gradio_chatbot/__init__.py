976ae15c47222bfd74a9d4700e04ccbde7b3d34a
Hiram Deng <72371329+dzhsurf@users.noreply.github.com>
2024-10-02 04:58:47 -0700

    Refactor example GradioChatBot class

--- examples/gradio_chatbot/__init__.py
+++ examples/gradio_chatbot/__init__.py
@@ -1,10 +1,19 @@
-from typing import Any, AsyncGenerator, Dict, List, Tuple
+import logging
+import multiprocessing
+import queue
+import time
+from abc import ABC, abstractmethod
+from typing import Any, AsyncGenerator, Dict, Generic, List, Optional, Tuple, TypeVar
 
 import gradio as gr
 from gradio_chatbot.utils import HookObjectAsyncMethod
 
+from flexiagent.task.task_node import FxTaskAgent, FxTaskEntity
 
-class GradioChatbot:
+logger = logging.getLogger(__name__)
+
+
+class GradioChatBot(ABC):
     def __init__(self):
         pass
 
@@ -57,3 +66,129 @@ class GradioChatbot:
 
     async def on_exit(self):
         return
+
+
+AgentInput = TypeVar("AgentInput", bound=FxTaskEntity)
+AgentOutput = TypeVar("AgentOutput", bound=FxTaskEntity)
+
+
+class AgentChatBot(GradioChatBot, Generic[AgentInput, AgentOutput]):
+    def __init__(self):
+        super().__init__()
+        self.result_dict: Dict[str, Optional[AgentOutput]] = {}
+        self.processing_manager = multiprocessing.Manager()
+        self.input_queue: queue.Queue[Optional[Tuple[str, AgentInput]]] = (
+            self.processing_manager.Queue()
+        )
+        self.output_queue: queue.Queue[Tuple[str, Optional[AgentOutput]]] = (
+            self.processing_manager.Queue()
+        )
+        self.worker_num = 1
+        self.worker_processor: List[multiprocessing.Process] = []
+        for i in range(self.worker_num):
+            worker = multiprocessing.Process(
+                target=self.__class__.worker_process,
+                args=(i, self.input_queue, self.output_queue),
+            )
+            self.worker_processor.append(worker)
+            worker.start()
+
+    @classmethod
+    @abstractmethod
+    def create_agent(cls) -> FxTaskAgent:
+        pass
+
+    @abstractmethod
+    def create_agent_input(
+        self, message: str, history: List[Dict[str, Any]]
+    ) -> AgentInput:
+        pass
+
+    @abstractmethod
+    def process_agent_output(self, response: Optional[AgentOutput]) -> str:
+        pass
+
+    @classmethod
+    def worker_process(
+        cls, i: int, input_queue: queue.Queue, output_queue: queue.Queue
+    ):
+        logger.info(f"Worker {i} start.")
+        try:
+            chatbot_agent = cls.create_agent()
+            while True:
+                input: Optional[Tuple[str, AgentInput]] = input_queue.get(block=True)
+                if input is None:
+                    break
+                if (
+                    isinstance(input, tuple)
+                    and isinstance(input[0], str)
+                    and isinstance(input[1], FxTaskEntity)
+                ):
+                    session_id = input[0]
+                    try:
+                        output = chatbot_agent.invoke(input[1])
+                        output_queue.put((session_id, output))
+                    except Exception as e:
+                        logger.error(e)
+                        output_queue.put((session_id, None))
+                else:
+                    raise ValueError(f"Input not match. {input}")
+        except KeyboardInterrupt:
+            pass
+        except Exception as e:
+            logger.error(e)
+        logger.info(f"Worker {i} exit.")
+
+    async def on_process_submit(
+        self, message: str, history: List[Dict[str, Any]]
+    ) -> AsyncGenerator[str, None]:
+        input = self.create_agent_input(message, history)
+        agent_response = await self._ask(input)
+        output = self.process_agent_output(agent_response)
+        yield output
+
+    async def on_exit(self):
+        self.input_queue.put(None)
+        for worker in self.worker_processor:
+            worker.join()
+
+    def _generate_task_id(self) -> str:
+        return f"{len(self.result_dict)+1}"
+
+    async def _launch_task(self, input: AgentInput) -> Optional[AgentOutput]:
+        task_id = self._generate_task_id()
+        self.input_queue.put((task_id, input))
+
+        # wait for response
+        logger.info(f"Wait for task_id: {task_id} input: {input} response...")
+        begin_time = time.time()
+        while True:
+            try:
+                output: Optional[Tuple[str, Optional[AgentOutput]]] = None
+                output = self.output_queue.get(block=False, timeout=5)
+                if (
+                    output
+                    and isinstance(output, tuple)
+                    and isinstance(output[0], str)
+                    and ((output[1] is None) or isinstance(output[1], FxTaskEntity))
+                ):
+                    output_task_id = output[0]
+                    output_response = output[1]
+                    self.result_dict[output_task_id] = output_response
+                    if task_id in self.result_dict:
+                        break
+            except queue.Empty:
+                pass
+            except Exception as e:
+                logger.error(e)
+                break
+        end_time = time.time()
+        cost = end_time - begin_time
+        logger.info(f"Get {task_id} response. cost: {cost}")
+        if task_id in self.result_dict:
+            response = self.result_dict.pop(task_id)
+            return response
+        return None
+
+    async def _ask(self, input: AgentInput) -> Optional[AgentOutput]:
+        return await self._launch_task(input)
--- examples/gradio_chatbot/simple_chatbot.py
+++ examples/gradio_chatbot/simple_chatbot.py
@@ -1,12 +1,8 @@
-import asyncio
 import logging
-import multiprocessing
 import os
-import queue
-import time
-from typing import Any, AsyncGenerator, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Optional
 
-from gradio_chatbot import GradioChatbot
+from gradio_chatbot import AgentChatBot
 from pydantic import BaseModel
 
 from flexiagent.llm.config import LLMConfig
@@ -22,7 +18,6 @@ logger = logging.getLogger(__name__)
 
 
 class ChatBotInput(FxTaskEntity):
-    session_id: int
     input: str
     history_as_text: str
 
@@ -68,156 +63,45 @@ def get_llm_config() -> LLMConfig:
     return llm_config
 
 
-def create_agent(llm_config: LLMConfig) -> FxTaskAgent:
-    instruction = """You are a chatbot assistant. Assist user and response user's question.
+class SimpleChatBot(AgentChatBot[ChatBotInput, ChatBotResponse]):
+    def __init__(self):
+        super().__init__()
+        self.session_history_max_limit = 30
+
+    @classmethod
+    def create_agent(cls) -> FxTaskAgent:
+        llm_config = get_llm_config()
+        instruction = """You are a chatbot assistant. Assist user and response user's question.
 
 {input.history_as_text}
 
 Question: {input.input}
 """
-    chatbot_agent = FxTaskAgent(
-        task_graph=[
-            FxTaskConfig(
-                task_key="output",
-                input_schema={"input": ChatBotInput},
-                output_schema=ChatBotResponse,
-                action=FxTaskAction(
-                    type="llm",
-                    act=FxTaskActionLLM(
-                        llm_config=llm_config,
-                        instruction=instruction,
+        chatbot_agent = FxTaskAgent(
+            task_graph=[
+                FxTaskConfig(
+                    task_key="output",
+                    input_schema={"input": ChatBotInput},
+                    output_schema=ChatBotResponse,
+                    action=FxTaskAction(
+                        type="llm",
+                        act=FxTaskActionLLM(
+                            llm_config=llm_config,
+                            instruction=instruction,
+                        ),
                     ),
                 ),
-            ),
-        ]
-    )
-    return chatbot_agent
-
-
-def worker_process(i: int, input_queue: queue.Queue, output_queue: queue.Queue):
-    logger.info(f"Worker {i} start.")
-
-    # worker logic
-    try:
-        chatbot_agent = create_agent(get_llm_config())
-        stop_flag = False
-        while not stop_flag:
-            input: Optional[ChatBotInput] = input_queue.get(block=True)
-            if input is None:
-                break
-            if isinstance(input, ChatBotInput):
-                try:
-                    session_id = input.session_id
-                    output = chatbot_agent.invoke(input)
-                    output_queue.put((session_id, output))
-                except Exception as e:
-                    logger.error(e)
-                    stop_flag = True
-            else:
-                raise ValueError(f"Input not match. {input}")
-    except KeyboardInterrupt:
-        pass
-    logger.info(f"Worker {i} exit.")
-
-
-class SimpleChatBot(GradioChatbot):
-    def __init__(self):
-        super().__init__()
-        self.session_list: List[Tuple[int, List[ChatMessage]]] = []
-        self.session_list_max_limit = 50
-        self.session_history_max_limit = 30
-        self.result_dict: Dict[str, ChatBotResponse] = {}
-
-        self.processing_manager = multiprocessing.Manager()
-        self.input_queue: queue.Queue[Optional[ChatBotInput]] = (
-            self.processing_manager.Queue()
+            ]
         )
-        self.output_queue: queue.Queue[Tuple[int, ChatBotResponse]] = (
-            self.processing_manager.Queue()
-        )
-        self.worker_num = 1
-        self.worker_processor: List[multiprocessing.Process] = []
-        for i in range(self.worker_num):
-            worker = multiprocessing.Process(
-                target=worker_process,
-                args=(i, self.input_queue, self.output_queue),
-            )
-            self.worker_processor.append(worker)
-            worker.start()
 
-    async def on_process_submit(
+        return chatbot_agent
+
+    def create_agent_input(
         self, message: str, history: List[Dict[str, Any]]
-    ) -> AsyncGenerator[str, None]:
-        # update history to session
-        session_id, history_as_text = self._update_session_history(history)
-        # create input and call agent
-        input = ChatBotInput(
-            session_id=session_id,
-            input=message,
-            history_as_text=history_as_text,
-        )
-        response: str = await self._ask(input)
-        yield response
-
-    async def on_exit(self):
-        self.input_queue.put(None)
-        for worker in self.worker_processor:
-            worker.join()
-
-    async def _launch_task(self, input: ChatBotInput) -> str:
-        self.input_queue.put(input)
-
-        # wait for response
-        key = f"{input.session_id}"
-        logger.info(
-            f"Wait for session_id: {input.session_id} input: {input.input} response..."
-        )
-        begin_time = time.time()
-        while True:
-            try:
-                output: Optional[Tuple[int, ChatBotResponse]] = None
-                output = self.output_queue.get(block=False)
-                if (
-                    output
-                    and isinstance(output, tuple)
-                    and isinstance(output[1], ChatBotResponse)
-                ):
-                    output_session_id = output[0]
-                    output_response = output[1]
-                    self.result_dict[f"{output_session_id}"] = output_response
-                    if key in self.result_dict:
-                        break
-            except queue.Empty:
-                pass
-            except Exception as e:
-                logger.error(e)
-                break
-            await asyncio.sleep(1)  # in seconds
-        end_time = time.time()
-        cost = end_time - begin_time
-        logger.info(f"Get session_id: {input.session_id} response. cost: {cost}")
-        if key in self.result_dict:
-            response: ChatBotResponse = self.result_dict[key]
-            return response.response
-        return ""
-
-    async def _ask(self, input: ChatBotInput) -> str:
-        return await self._launch_task(input)
-
-    def _update_session_history(self, history: List[Dict[str, Any]]) -> Tuple[int, str]:
-        # generate next session id
-        if len(self.session_list) > 0:
-            last_session = self.session_list[-1]
-            session_id = last_session[0] + 1
-        else:
-            session_id = 1
+    ) -> ChatBotInput:
         # change histroy to chatmessage list
         session_message = [ChatMessage.model_validate(msg) for msg in history]
         session_message = session_message[-self.session_history_max_limit :]
-        # update to session
-        self.session_list.append((session_id, session_message))
-        if len(self.session_list) > self.session_list_max_limit:
-            self.session_list.pop(0)
 
         # generate history as text
         history_as_text = "History conversation:\n"
@@ -226,4 +110,12 @@ class SimpleChatBot(GradioChatbot):
         if len(history) == 0:
             history_as_text += "No history conversation."
 
-        return (session_id, history_as_text)
+        return ChatBotInput(
+            input=message,
+            history_as_text=history_as_text,
+        )
+
+    def process_agent_output(self, response: Optional[ChatBotResponse]) -> str:
+        if response:
+            return response.response
+        return "[Agent without any output]"
